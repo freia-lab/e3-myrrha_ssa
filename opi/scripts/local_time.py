6180e96c33cd9b9f508952d946a1988cd56fc37d
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2026-02-03 15:18:27 +0100

    All OPIs but "Buffer" tab migrated to Phoebus.

--- opi/scripts/local_time.py
+++ opi/scripts/local_time.py
@@ -1,9 +1,25 @@
-from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.display.builder.runtime.script import PVUtil, ScriptUtil
 import time
-import sys
 
-value = PVUtil.getLong(pvs[0])
-my_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(value))
-value = PVUtil.getLong(pvs[1])
-my_time = my_time + " " + str(value/1000) + u"\u03BCs"
-widget.setValue(my_time)
+# 1. Get the list of PVs assigned to this script
+# In Phoebus, 'pvs' is still available if you defined them in the script properties
+try:
+    # Get values using Phoebus PVUtil
+    raw_seconds = PVUtil.getLong(pvs[0])
+    raw_micros = PVUtil.getLong(pvs[1])
+
+    # 2. Format the Date/Time
+    formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(raw_seconds))
+
+    # 3. Handle Microseconds and the Greek Mu symbol (u"\u03BC")
+    # Using format() is cleaner for Jython string concatenation
+    micro_val = str(raw_micros / 1000)
+    final_string = u"{} {} {}\u03BCs".format(formatted_time, micro_val, "")
+
+    # 4. Update the widget. In Phoebus, most text widgets use the "text" property.
+    widget.setPropertyValue("text", final_string)
+
+except Exception as e:
+    # Log errors to the Phoebus console for debugging
+    print("Error in timestamp script: " + str(e))
+    
