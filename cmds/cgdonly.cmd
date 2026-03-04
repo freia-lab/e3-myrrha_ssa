a3eab3ee89f93599153138c49b4d9d8f65712742
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2026-03-04 16:26:16 +0100

    Changed SCAN for EtherCAT status PVs from I/O Intr to 1 second (I/O Intr didn't work)

--- cmds/cgdonly.cmd
+++ cmds/cgdonly.cmd
@@ -9,11 +9,10 @@ epicsEnvSet(DISCIPLINE, "RFS")
 epicsEnvSet(CAB_SSA, "CAB-$(CAB_NUM)")
 epicsEnvSet(CAB_IOC, "CAB-$(IOC_CAB_NUM)")
 
-
 epicsEnvSet(PREFIX_ECAT, "$(CAB_IOC):$(DISCIPLINE)-IOC")
 iocshLoad("$(ecat2_DIR)ecat2.iocsh", "ECAT_FREQ=100")
-ecat2sts d0.r0 d0.r249
-ecat2sts d1.r0 d1.r249
+#ecat2sts d0.r0 d0.r249
+#ecat2sts d1.r0 d1.r249
 
 dbLoadRecords("$(myrrha_ssa_DB)ecat.db", "PREFIX=$(PREFIX_ECAT),IOC_NUM=$(IOC_NUM)")
 
